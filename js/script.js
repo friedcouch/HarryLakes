11859e5fc45f0773f9261805e6340db866954815
friedcouch <S10203108@connect.np.edu.sg>
2020-11-28 12:04:54 +0800

    Changed id 'navbar' to 'navbar-header'

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ var prevScrollPos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollPos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar-header").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-80px";
+    document.getElementById("navbar-header").style.top = "-80px";
   }
   prevScrollPos = currentScrollPos;
 }
