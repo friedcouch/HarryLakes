723a8f45ab1015edd55ed05cb2314e6099451275
friedcouch <S10203108@connect.np.edu.sg>
2020-11-28 09:49:21 +0800

    Added js/script.js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var prevScrollPos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-80px";
+  }
+  prevScrollPos = currentScrollPos;
+}
